bf01f5976299c0af85378715f951fbda3a05fd65
siddharthnahata <siddharthnahata492@gmail.com>
2025-05-10 20:02:19 +0530

    Add this file

---
+++ Day-5/day_5_extra1.py
@@ -0,0 +1,11 @@
+nums = [1, 10, 98, 15, 26, 99, 101, 111, 21, 7]
+
+max_num = 0
+
+for num in nums:
+    if num > max_num:
+        max_num = num
+        
+print(max(nums))
+print(max_num)
+
---
+++ Day-5/day_5_extra2.py
@@ -0,0 +1,5 @@
+sum_num = 0
+
+for num in range(101):
+    sum_num += num
+print(sum_num)
